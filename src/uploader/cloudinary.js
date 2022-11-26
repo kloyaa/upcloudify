1809fa9f406fc474695e67364099d28d31e82352
Kolya Madridano <58349866+kloyaa@users.noreply.github.com>
2022-11-26 21:38:59 +0800

    chore! remove typescript (#6)
    Co-authored-by: Kolya Madridano <58349866+iluvkoohi@users.noreply.github.com>

---
+++ src/uploader/cloudinary.js
@@ -0,0 +1,10 @@
+class Cloudinary {
+    async upload() {
+         return {
+             message: 'type === UploadType.SingleImage',
+             description: 'should return url of uploaded image'
+         };
+     }
+}
+
+module.exports = Cloudinary;
